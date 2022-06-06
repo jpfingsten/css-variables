b6c4c9d7dd3ac16e8f78e4318fecfbdf00125ef7
jpfingsten <jasmine.pfingsten@gmail.com>
2022-06-06 10:14:23 -0400

    Added JS to change styles

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || "";
+    console.log(this.name);
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(inputs => inputs.addEventListener("change", handleUpdate));
+inputs.forEach(inputs => inputs.addEventListener("mousemove", handleUpdate));
